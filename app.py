0441dacfe7213cc67bb79345507e122a60a2d443
Muhammad Afzal <afzal2000@yahoo.com>
2020-08-09 15:49:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 import os
 
 from flask import Flask
+from flask import request
+from pyspark.sql import SparkSession
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+
+def produce_pi(scale):
+    spark = SparkSession.builder.appName("PythonPi").getOrCreate()
+    n = 100000 * scale
+
+    def f(_):
+        from random import random
+        x = random()
+        y = random()
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(
+        range(1, n + 1), scale).map(f).reduce(lambda x, y: x + y)
+    spark.stop()
+    pi = 4.0 * count / n
+    return pi
+
+
+@app.route("/")
+def index():
+    return "Python Flask SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app."
+
+
+@app.route("/sparkpi")
+def sparkpi():
+    scale = int(request.args.get('scale', 2))
+    pi = produce_pi(scale)
+    response = "Pi is roughly {}".format(pi)
+
+    return response
 
 
 if __name__ == "__main__":
